8224a8515e740e995ccaa1be305f245ec1b9ee9d
linden-gan <lg57@uw.edu>
2022-05-15 16:22:37 -0700

    Tahoma done

--- aurmr/src/robot_api/tahoma.py
+++ aurmr/src/robot_api/tahoma.py
@@ -14,6 +14,7 @@ import pybullet_planning as pp
 
 import os
 from termcolor import cprint
+import copy
 
 from move import plan_motion_from_to, sim_execute_motion
 
@@ -42,11 +43,28 @@ THRESHOLD = 0.1
 
 
 class Tahoma:
-    def __init__(self, pb_robot, initial_joint_state=PYBULLET_JOINT_INITIAL) -> None:
-        # initialize arm info
-        self._pb_robot = pb_robot
+    def __init__(self) -> None:
+        # initialize robot in pybullet
+        p.connect(p.GUI)
+        p.setGravity(0, 0, -9.8)
+        self._pb_robot = p.loadURDF(ROBOT_URDF)
+
+        # initialize arm meta info
         self._joint_indices = pp.joints_from_names(pb_robot, JOINT_NAMES)
         self._disabled_links = pp.get_disabled_collisions(pb_robot, SELF_COLLISION_DISABLED_LINKS)
+
+        # initialize joint state in simulator
+        self.sim_joint_state = initial_joint_state
+        # initialize joint state of actual robot
+        self.actual_joint_state = None
+
+        # listen to robot's joint state
+        self._robot_state_sub = rospy.Subscriber(ACTUAL_STATE_TOPIC, JointState, callback=self.get_actual_state)
+
+        # initialize action client to move robot's arm
+        self._trajectory_client = actionlib.SimpleActionClient(JOINT_ACTION_SERVER, FollowJointTrajectoryAction)
+
+        sleep(1)
 
         # synchronize pybullet simulator's initial pose to actual robot if actual state is not
         # equal to simulator's state
@@ -59,31 +77,17 @@ class Tahoma:
                 cprint('Bad initial joint state', 'red')
                 return
             sim_execute_motion(self._pb_robot, self._joint_indices, path)
-        # initialize joint state in simulator
-        self._sim_joint_state = initial_joint_state
-        # initialize joint state of actual robot
-        self._actual_joint_state = None
-
-        # listen to robot's joint state
-        self._robot_state_sub = rospy.Subscriber(ACTUAL_STATE_TOPIC, JointState, callback=self.get_actual_state)
-
-        # initialize action client to move robot's arm
-        self._trajectory_client = actionlib.SimpleActionClient(JOINT_ACTION_SERVER, FollowJointTrajectoryAction)
 
     def get_actual_state(self, msg: JointState):
-        self._actual_joint_state = msg.position
+        self.actual_joint_state = msg.position
 
     def synchronize(self):
         # synchronize pybullet simulator's initial pose to actual robot if actual state is not
         # equal to simulator's state
-        accurate = True
-        for i in range(len(self._actual_joint_state)):
-            if abs(self._actual_joint_state[i] - self._sim_joint_state[i]) > THRESHOLD:
-                accurate = False
-                break
+        accurate = state_all_close(self.actual_joint_state.copy(), self.sim_joint_state.copy())
 
         if not accurate:
-            path = plan_motion_from_to(robot, self._joint_indices, self._sim_joint_state, self._actual_joint_state,
+            path = plan_motion_from_to(robot, self._joint_indices, self.sim_joint_state, self.actual_joint_state,
                                        obstacles=[], self_collisions=True,
                                        disabled_collisions=self._disabled_links,
                                        algorithm='rrt')
@@ -93,7 +97,7 @@ class Tahoma:
 
             sim_execute_motion(self._pb_robot, self._joint_indices, path)
             # update initialized joint state
-            self._sim_joint_state = path[-1]
+            self.sim_joint_state = path[-1]
 
     def move_to_pose_goal(self,
                           end_pose: PoseStamped,
@@ -109,9 +113,9 @@ class Tahoma:
                                                        position, orien)
 
         # plan motion
-        print(f'start conf is {self._sim_joint_state}')
+        print(f'start conf is {self.sim_joint_state}')
         print(f'end conf is {end_joint_state}')
-        path = plan_motion_from_to(robot, self._joint_indices, self._sim_joint_state, end_joint_state,
+        path = plan_motion_from_to(robot, self._joint_indices, self.sim_joint_state, end_joint_state,
                                    obstacles=[], self_collisions=True,
                                    disabled_collisions=self._disabled_links,
                                    algorithm='rrt')
@@ -130,19 +134,28 @@ class Tahoma:
         # send goal
         self._trajectory_client.send_goal(goal)
         self._trajectory_client.wait_for_result(rospy.Duration(execution_timeout))
-        # return result
-        result = self._trajectory_client.get_result()
-        print(result)
-        if result is not None:
-            cprint(f'error code is {result.error_code}', 'cyan')
-            if result.error_code == 0:
-                # update current joint state
-                self._sim_joint_state = end_joint_state
-                self.synchronize()
-                return True
+        # # return result
+        # result = self._trajectory_client.get_result()
+        # print(result)
+        # if result is not None:
+        #     cprint(f'error code is {result.error_code}', 'cyan')
+        #     if result.error_code == 0:
+        #         # update current joint state
+        #         self.sim_joint_state = end_joint_state
+        #         self.synchronize()
+        #         return True
 
-        cprint('Failed to move actual robot', 'red')
-        return False
+        # cprint('Failed to move actual robot', 'red')
+        self.synchronize()
+        return state_all_close(self.actual_joint_state.copy(), end_joint_state)
+
+
+def state_all_close(state1: list, state2: list):
+    assert len(state1) == len(state2)
+    for i in range(len(state)):
+        if abs(state1[i] - state2[i]) > THRESHOLD:
+            return False
+    return True
 
 
 def to_joint_trajectory_point(waypoints):
@@ -154,19 +167,3 @@ def to_joint_trajectory_point(waypoints):
         res.append(JointTrajectoryPoint(positions=point))
 
     return res
-
-
-if __name__ == "__main__":
-    rospy.init_node('arm')
-
-    # initialize the env
-    p.connect(p.GUI)
-    p.setGravity(0, 0, -9.8)
-    robot = p.loadURDF(ROBOT_URDF)
-
-    # initialize arm robot
-    arm = Tahoma(robot, [-0.8574946697157134, 0.3600245844803187, -1.3603905625549857, -0.5915446627350632, 1.5741277934974172, 0.856103511284937])
-
-    # (-0.8574946697157134, 0.3600245844803187, -1.3603905625549857, -0.5915446627350632, 1.5741277934974172, 0.856103511284937)
-
-    rospy.spin()
